b298add35380ec6bbef694dacd342e56209108a9
nikitha <nikithalakmarapu@gmail.com>
2020-02-21 15:48:57 -0600

    committed map reduce for the sum of the volumes

---
+++ nikitha-sum-of-prices/reducer.py
@@ -0,0 +1,30 @@
+s = open("02.txt","r")
+r = open("03.txt", "w")
+
+thisKey = ""
+thisValue = 0.0
+count=0
+
+for line in s:
+  data = line.strip().split('\t')
+  region, totalvolume = data
+  if region != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      r.write(thisKey + '\t' + str(thisValue)+'\n')
+    # start over when changing keys
+    thisKey = region 
+    thisValue = 0.0
+  # apply the aggregation function - now maximum
+  thisValue += float(totalvolume)
+  #print(thisKey + '\t' + str(thisValue)+'\n')
+  #if (float(totalvolume) > thisValue):
+      #thisValue =float(totalvolume)
+    
+# output the final entry when done
+r.write(thisKey + '\t' + str(thisValue)+'\n')
+#print("count ",count)
+
+
+s.close()
+r.close()
